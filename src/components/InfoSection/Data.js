f0f73b7870e0534b5bf9b440fd25ebe845905ed8
Pascariu Cosmin <cojo18@MacBook-Air-Pascariu-3.local>
2022-02-10 14:59:44 +0200

    Add about and discover pages

--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -9,8 +9,46 @@ export const homeObjOne = {
   description:
     "Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.",
   buttonLabel: "Get started",
-  imgStart: false,
+  imgStart: true,
   img: require("../../images/svg-2.png"),
+  alt: "Car",
+  dark: true,
+  primary: true,
+  darkText: false,
+};
+
+export const homeObjTwo = {
+  //Second data section
+  id: "discover",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "Unlimited Access",
+  headline: "Unlimited Transactions with zero fees",
+  description:
+    "Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.",
+  buttonLabel: "Learn More",
+  imgStart: true,
+  img: require("../../images/svg-5.png"),
+  alt: "Piggybank",
+  dark: false,
+  primary: false,
+  darkText: true,
+};
+
+export const homeObjThree = {
+  //Third data section
+  id: "about",
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Premium Bank",
+  headline: "Unlimited Transactions with zero fees",
+  description:
+    "Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.",
+  buttonLabel: "Get started",
+  imgStart: true,
+  img: require("../../images/svg-4.svg"),
   alt: "Car",
   dark: true,
   primary: true,
